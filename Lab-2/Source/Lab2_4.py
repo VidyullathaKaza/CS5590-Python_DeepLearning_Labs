56e3f7f85c1529b3f14a68524036bb2896ea61e4
VidyullathaKaza <42659700+VidyullathaKaza@users.noreply.github.com>
2019-07-27 10:08:39 -0500

    Changes

--- Lab-2/Source/Lab2_4.py
+++ Lab-2/Source/Lab2_4.py
@@ -5,11 +5,10 @@ from sklearn.feature_extraction.text import CountVectorizer
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential,Model
-from keras.layers import Input,Dense, Embedding, LSTM, SpatialDropout1D,Dropout, Conv1D, GlobalMaxPooling1D
+from keras.layers import Input,Dense, Embedding, LSTM, SpatialDropout1D,Dropout
 from sklearn.model_selection import train_test_split
 from keras.utils.np_utils import to_categorical
 import re
-from keras.optimizers import adam
 
 data = pd.read_csv('spam.csv',encoding='latin-1')
 # Keeping only the neccessary columns
@@ -23,8 +22,8 @@ data['v2'] = data['v2'].apply((lambda x: re.sub('[^a-zA-z0-9\s]', '', x)))
 for idx, row in data.iterrows():
     row[0] = row[0].replace('rt', ' ')
 
-max_features = 2000
-tokenizer = Tokenizer(num_words=max_features, split=' ')
+max_fatures = 2000
+tokenizer = Tokenizer(num_words=max_fatures, split=' ')
 tokenizer.fit_on_texts(data['v2'].values)
 X = tokenizer.texts_to_sequences(data['v2'].values)
 print(X)
@@ -33,15 +32,18 @@ print(X)
 embed_dim = 128
 
 from sklearn.preprocessing import LabelEncoder
-
+lstm_out = 196
 def createmodel():
      model = Sequential()
+     model.add(Embedding(max_fatures, embed_dim,input_length = X.shape[1]))
+     model.add(SpatialDropout1D(0.4))
+     model.add(LSTM(lstm_out, dropout=0.5, recurrent_dropout=0.5))
      model.add(Dense(512, activation='relu'))
      model.add(Dense(256, activation='tanh'))
      model.add(Dense(2, activation='softmax'))
-     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+     model.compile(loss = 'categorical_crossentropy', optimizer='adam',metrics = ['accuracy'])
      return model
- #    model.summary()
+# print(model.summary())
 
 labelencoder = LabelEncoder()
 integer_encoded = labelencoder.fit_transform(data['v1'])
